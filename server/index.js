44a742358142cb9c5926a4739e801e0fb5ab9595
Barak <Barak.Chamo@Gmail.com>
2015-06-27 01:52:05 +0100

    updated CORS settings

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 var app = require('express')();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/quotes', require('./routes/quotes'));
+
+app.post('/upload', require('./routes/upload'));
 
 var server = app.listen(3000, function () {
   var address = server.address();
