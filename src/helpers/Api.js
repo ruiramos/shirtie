3a20c6563b71dd8cf9aafd2a5eb28ba41cf7f3f2
Rui Ramos <rui@ometria.com>
2015-06-27 02:15:05 +0100

    api

--- src/helpers/Api.js
+++ src/helpers/Api.js
@@ -6,8 +6,7 @@ var host = 'http://localhost:3000';
 module.exports = {
   postImage: function(image, callback){
     request.post(host + '/upload')
-      .type('png')
-      .send(image)
+      .send({image: image})
       .end(callback);
   }
 }
