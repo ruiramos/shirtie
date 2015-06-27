84e00a8756c6610030159e3dc7c5bcc4ea8c7b08
Rui Ramos <rui@ometria.com>
2015-06-27 16:42:23 +0100

    imagegenerator

--- server/controllers/imageGenerator.js
+++ server/controllers/imageGenerator.js
@@ -1,18 +1,38 @@
 
 var fs = require('fs'),
-    gm = require('./gm').subClass({imageMagick: true});
+    gm = require('gm').subClass({imageMagick: true}),
+    path = require('path');
+
+var dir = path.resolve(__dirname, '../../uploads') + '/' ;
 
 var ImageGenerator = {
-  generateImage: function(imgPath, quote){
+  getResizedStream: function(imgName, writeStream){
+    gm(dir + imgName)
+    .resize(600, 600)
+    .stream()
+    .pipe(writeStream);
+  },
 
-    gm(imgPath)
+  generateImage: function(imgName, quote, author){
+    var marginBottom = 50,
+        lineHeight = 40;
+
+    gm(dir + imgName)
       .resize(600, 600)
-      .extent(600, 800)
-      .font("Helvetica.ttf", 12)
-      .drawText(600, 20, "GMagick!")
-      .write('/upload/new.png', function (err) {
-        if (!err) console.log('done');
-      });
+      .size(function(err, size){
+        gm(dir + imgName)
+          .extent(size.width, size.height + marginBottom + lineHeight * 2)
+          .font("Helvetica", 52)
+          .stroke('#000')
+          .strokeWidth(1)
+          .drawText(20, size.height + marginBottom, quote)
+          .font("Arial", 30)
+          .drawText(20, size.height + marginBottom + lineHeight, author)
+          .write(dir + 'new.png', function (err) {
+            if (!err) console.log('done');
+            else console.log(err)
+          });
+      })
 
   }
 }
