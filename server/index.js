dc3ab4c611672cbd3397565bb59daf12b036786a
Barak <Barak.Chamo@Gmail.com>
2015-06-27 01:55:36 +0100

    updated server deps

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
-var app = require('express')();
+var app = require('express')(),
+    bodyParser = require('body-parser');
 
+// CORS Handling
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
-app.get('/quotes', require('./routes/quotes'));
+// Parse body in post requests
+app.use(bodyParser);
 
+// Endpoints
+app.get('/quotes', require('./routes/quotes'));
 app.post('/upload', require('./routes/upload'));
 
 var server = app.listen(3000, function () {
