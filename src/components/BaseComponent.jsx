ef652867d325000acb7f794325d4e2cdccd7082d
Rui Ramos <rui@ometria.com>
2015-06-28 12:03:26 +0100

    asd

--- src/components/BaseComponent.jsx
+++ src/components/BaseComponent.jsx
@@ -3,6 +3,6 @@ var React = require('react');
 
 export class BaseComponent extends React.Component {
   bindMethods(...methods){
-    methods.forEach( (method) => this[method] = this[method].bind(this) );
+    methods.forEach((method) => this[method] = this[method].bind(this) );
   }
 }
--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
 export class Confirmation extends React.Component {
+  constructor(){
+    super()
+  }
+
   render() {
+    if(this.props.formData){
+      var formDetails = this.props.formData.map((field) => {
+        return (
+          <div className="field">
+            <label>{field.name}</label>
+            <p>{field.value}</p>
+          </div>
+        );
+      })
+    } else {
+      var formDetails = [];
+    }
+
     return (
-      <div className={this.props.classes}>all ok! your id: {this.props.orderId}</div>
+      <div>
+        <span className="card-title activator grey-text text-darken-4">Order placed!<i className="material-icons right red-text">photo</i></span>
+        <p>Your order is on its way.</p>
+
+        <p>Here are your order details: {this.props.orderId}</p>
+        {formDetails}
+      </div>
+
     );
   }
 }
