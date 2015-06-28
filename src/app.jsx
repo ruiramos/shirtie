67d174356998af1fd12993efeb211f694c71d33e
Barak <Barak.Chamo@Gmail.com>
2015-06-28 13:26:12 +0100

    styling updates

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,6 @@ var React = require('react'),
     App = require('./components/App.jsx').App;
 
 window.React = React;
-window.host = 'http://www.shirtie.co:3210';
+window.host = (window.location.host.indexOf('localhost')>=0) ? 'http://localhost:3210' : 'http://www.shirtie.co:3210';
 
 React.render(<App />, document.body);
